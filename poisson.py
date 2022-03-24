73204005635d24e686c271a6d114d8311282c1c1
paulgute <paulgute24@gmail.com>
2022-03-24 16:06:16 +0800

    update

--- poisson.py
+++ poisson.py
@@ -34,7 +34,7 @@ def Poisson1(rng):
     ret = 1
     a = 1
     b = 0
-    while true:
+    while True:
         j = rng.randrange(a)
         if j < a and j < b: return ret
         if j == a:
@@ -71,16 +71,20 @@ def main(argv):
     size = 10000
     test_index = 0
     overall_time = 0
+    print('benchmarking time for generating poisson..... ')
     while test_index < num_runs:
         start = time.time()
 
-        samples_1 = [Poissonint(mx,my) for i in range(2*size)]
+        samples_1 = [PoissonInt(mx,my) for i in range(2*size)]
         samples = [samples_1[i]-samples_1[i+size] for i in range(size)]
 
         end = time.time()
         elapsed_time = end - start
-        print('generated ', num_generated, ' samples in ', elapsed_time, ' seconds.' )
+        print('generated ', size, ' samples in ', elapsed_time, ' seconds.' )
         overall_time = overall_time + elapsed_time
         test_index = test_index + 1
 
-    print('On average, generated ', num_generated, ' samples in ', overall_time/num_runs, ' seconds.' )
+    print('On average, generated ', size, ' samples in ', overall_time/num_runs, ' seconds.' )
+
+if __name__ == '__main__':
+    app.run(main)
